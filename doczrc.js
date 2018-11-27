1a439cc6a5a44e3cfe8031c164ed7c8805222c1f
George Lima <georgelima11@hotmail.com>
2018-11-26 23:52:29 -0300

    feature: setup docz

---
+++ doczrc.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { css } from 'docz-plugin-css';
+
+export default {
+  title: 'ZCash',
+  description: 'ZCash UI Documentation',
+  plugins: [css()],
+};
