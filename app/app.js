ef0bff7916eb54a3c909efb69c5bbeaca379cfa8
George Lima <georgelima11@hotmail.com>
2018-11-28 19:03:54 -0300

    feature: wrap root component with ThemeProvider

--- app/app.js
+++ app/app.js
@@ -3,15 +3,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { ThemeProvider } from 'styled-components';
+
 import { configureStore, history } from './store/configure';
 import { Router } from './router/container';
+import theme from './theme';
 
 const store = configureStore({});
 
 export default () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
     </ConnectedRouter>
   </Provider>
 );
