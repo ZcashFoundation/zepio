7fa36078dc61a4976ddbff11214bdafe74cc35ba
George Lima <georgelima11@hotmail.com>
2018-12-01 02:21:12 -0300

    hotfix: binary path and add -testnet on development env

--- config/daemon/zcashd-child-process.js
+++ config/daemon/zcashd-child-process.js
@@ -2,15 +2,20 @@
 import cp from 'child_process';
 import path from 'path';
 import processExists from 'process-exists';
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+import isDev from 'electron-is-dev';
 import type { ChildProcess } from 'child_process';
 
 import getBinariesPath from './get-binaries-path';
+import getOsFolder from './get-os-folder';
 import getDaemonName from './get-daemon-name';
 import fetchParams from './run-fetch-params';
 import log from './logger';
 
+const getDaemonOptions = () => (isDev ? ['-daemon', '-testnet'] : ['-daemon']);
+
 const runDaemon: () => Promise<?ChildProcess> = () => new Promise((resolve, reject) => {
-  const processName = path.join(getBinariesPath(), getDaemonName());
+  const processName = path.join(getBinariesPath(), getOsFolder(), getDaemonName());
 
   fetchParams()
     .then(() => {
@@ -20,7 +25,7 @@ const runDaemon: () => Promise<?ChildProcess> = () => new Promise((resolve, reje
           log('Already is running!');
           resolve();
         } else {
-          const childProcess = cp.spawn(processName, ['-daemon']);
+          const childProcess = cp.spawn(processName, getDaemonOptions());
 
           childProcess.stdout.on('data', (data) => {
             log(data.toString());
