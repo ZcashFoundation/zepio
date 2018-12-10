51893c2571ffa2c04555e51cc55c39149713189e
George Lima <georgelima11@hotmail.com>
2018-12-10 14:32:44 -0300

    test: add wallet-summary reducer tests

--- __tests__/actions/WalletSummary.test.js
+++ __tests__/actions/WalletSummary.test.js
@@ -2,7 +2,14 @@
 
 import configureStore from 'redux-mock-store';
 
-import { LOAD_WALLET_SUMMARY, loadWalletSummary } from '../../app/redux/modules/wallet';
+import {
+  LOAD_WALLET_SUMMARY,
+  LOAD_WALLET_SUMMARY_SUCCESS,
+  LOAD_WALLET_SUMMARY_ERROR,
+  loadWalletSummary,
+  loadWalletSummarySuccess,
+  loadWalletSummaryError,
+} from '../../app/redux/modules/wallet';
 
 const store = configureStore()();
 
@@ -15,6 +22,38 @@ describe('WalletSummary Actions', () => {
     expect(store.getActions()[0]).toEqual(
       expect.objectContaining({
         type: LOAD_WALLET_SUMMARY,
+      }),
+    );
+  });
+
+  test('should create an action to load wallet summary', () => {
+    const payload = {
+      total: 5000,
+      transparent: 5000,
+      shielded: 5000,
+    };
+
+    store.dispatch(loadWalletSummarySuccess(payload));
+
+    expect(store.getActions()[0]).toEqual(
+      expect.objectContaining({
+        type: LOAD_WALLET_SUMMARY_SUCCESS,
+        payload,
+      }),
+    );
+  });
+
+  test('should create an action to load wallet summary with error', () => {
+    const payload = {
+      error: 'Something went wrong!',
+    };
+
+    store.dispatch(loadWalletSummaryError(payload));
+
+    expect(store.getActions()[0]).toEqual(
+      expect.objectContaining({
+        type: LOAD_WALLET_SUMMARY_ERROR,
+        payload,
       }),
     );
   });
