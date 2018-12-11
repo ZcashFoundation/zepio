7d256e73b6807da176463b13dfeb316b53cf0104
George Lima <georgelima11@hotmail.com>
2018-12-10 21:40:17 -0300

    hotfix: make layout flex

--- app/components/layout.js
+++ app/components/layout.js
@@ -4,6 +4,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100vh;
   background: ${props => props.theme.colors.secondary};
