922c62eb385604e3b26f8d6f9bbd0b23282428ae
George Lima <georgelima11@hotmail.com>
2018-12-12 16:04:04 -0300

    feature: update icons in sidebar constants

--- app/constants/sidebar.js
+++ app/constants/sidebar.js
@@ -1,9 +1,16 @@
 // @flow
-import React from 'react';
-import { FaThLarge, FaCode } from 'react-icons/fa';
-import { IoIosSend } from 'react-icons/io';
-import { TiDownload } from 'react-icons/ti';
-import { MdSettings, MdTransform } from 'react-icons/md';
+import DashboardIcon from '../assets/images/dashboard_icon.svg';
+import DashboardIconActive from '../assets/images/dashboard_icon_active.svg';
+import ConsoleIcon from '../assets/images/console_icon.svg';
+import ConsoleIconActive from '../assets/images/console_icon_active.svg';
+import SendIcon from '../assets/images/send_icon.svg';
+import SendIconActive from '../assets/images/send_icon_active.svg';
+import ReceiveIcon from '../assets/images/receive_icon.svg';
+import ReceiveIconActive from '../assets/images/receive_icon_active.svg';
+import TransactionsIcon from '../assets/images/transactions_icon.svg';
+import TransactionsIconActive from '../assets/images/transactions_icon_active.svg';
+import SettingsIcon from '../assets/images/settings_icon.svg';
+import SettingsIconActive from '../assets/images/settings_icon_active.svg';
 
 import {
   DASHBOARD_ROUTE,
@@ -18,31 +25,31 @@ export const MENU_OPTIONS = [
   {
     label: 'Dashboard',
     route: DASHBOARD_ROUTE,
-    icon: <FaThLarge />,
-  },
-  {
-    label: 'Console',
-    route: CONSOLE_ROUTE,
-    icon: <FaCode />,
+    icon: (isActive: boolean) => (isActive ? DashboardIconActive : DashboardIcon),
   },
   {
     label: 'Send',
     route: SEND_ROUTE,
-    icon: <IoIosSend />,
+    icon: (isActive: boolean) => (isActive ? SendIconActive : SendIcon),
   },
   {
     label: 'Receive',
     route: RECEIVE_ROUTE,
-    icon: <TiDownload />,
+    icon: (isActive: boolean) => (isActive ? ReceiveIconActive : ReceiveIcon),
   },
   {
     label: 'Transactions',
     route: TRANSACTIONS_ROUTE,
-    icon: <MdTransform />,
+    icon: (isActive: boolean) => (isActive ? TransactionsIconActive : TransactionsIcon),
   },
   {
     label: 'Settings',
     route: SETTINGS_ROUTE,
-    icon: <MdSettings />,
+    icon: (isActive: boolean) => (isActive ? SettingsIconActive : SettingsIcon),
+  },
+  {
+    label: 'Console',
+    route: CONSOLE_ROUTE,
+    icon: (isActive: boolean) => (isActive ? ConsoleIconActive : ConsoleIcon),
   },
 ];
