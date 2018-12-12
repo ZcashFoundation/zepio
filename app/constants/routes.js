efe32a52276764bd0f364953197b62ae2ed2b756
George Lima <georgelima11@hotmail.com>
2018-12-11 21:09:22 -0300

    hotfix: fix route typo

--- app/constants/routes.js
+++ app/constants/routes.js
@@ -4,5 +4,5 @@ export const DASHBOARD_ROUTE = '/';
 export const CONSOLE_ROUTE = '/console';
 export const SEND_ROUTE = '/send';
 export const RECEIVE_ROUTE = '/receive';
-export const TRANSACTIONS_ROUTE = '/receive';
+export const TRANSACTIONS_ROUTE = '/transactions';
 export const SETTINGS_ROUTE = '/settings';
