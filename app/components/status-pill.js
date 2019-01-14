37fcba6c2daa8ca676e065e17db5f73e33f12713
eliabejr <eliabejunior@hotmail.com.br>
2019-01-14 09:09:11 -0300

    feat(status-pill): refactor component logic

--- app/components/status-pill.js
+++ app/components/status-pill.js
@@ -1,12 +1,12 @@
 // @flow
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import { TextComponent } from './text';
 
 import readyIcon from '../assets/images/green_check.png';
 import syncIcon from '../assets/images/sync_icon.png';
-import errorIcon from '../assets/images/error_icon.png';
+// import errorIcon from '../assets/images/error_icon.png';
 
 
 const Wrapper = styled.div`
@@ -34,45 +34,44 @@ type Props = {
 };
 
 type State = {
-  progress: number,
   type: string,
   icon: string,
-  isRunning: boolean,
 };
 
 export class StatusPill extends Component<Props, State> {
+  static defaulProps = {
+    percent: 0,
+  };
 
   state = {
-    progress: 0,
     type: 'synching',
     icon: syncIcon,
-    isRunning: false,
   };
 
   componentDidMount() {
-    this.status(this.props.percent);
-  };
+    const { percent } = this.props;
 
-  status = (percent) => {
-    if (percent === 100.0) {
+    this.status(percent);
+  }
+
+  status = (progress: number) => {
+    if (progress === 100.0) {
       this.setState(() => ({ type: 'ready' }));
       this.setState(() => ({ icon: readyIcon }));
     }
-  };
-  
+  }
+
   render() {
     const { type, icon } = this.state;
     const { percent } = this.props;
 
-    
     const showPercent = percent > 0 && percent < 100.0 ? `(${percent}%)` : '';
-    
+
     return (
       <Wrapper>
         <Icon src={icon} />
         <StatusPillLabel value={`${type} ${showPercent}`} />
       </Wrapper>
     );
-  };
-
-};
+  }
+}
