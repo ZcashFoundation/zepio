b6b3549cc89ad41a1a77002e00c6cf3760f2c10e
George Lima <georgelima11@hotmail.com>
2019-01-21 15:37:04 -0300

    feat(receive): load addresses in receive view container

---
+++ app/redux/modules/receive.js
@@ -0,0 +1,49 @@
+// @flow
+import type { Action } from '../../types/redux';
+
+// Actions
+export const LOAD_ADDRESSES_SUCCESS = 'LOAD_ADDRESSES_SUCCESS';
+export const LOAD_ADDRESSES_ERROR = 'LOAD_ADDRESSES_ERROR';
+
+export const loadAddressesSuccess = ({
+  addresses,
+}: {
+  addresses: string[],
+}) => ({
+  type: LOAD_ADDRESSES_SUCCESS,
+  payload: {
+    addresses,
+  },
+});
+
+export const loadAddressesError = ({ error }: { error: string }) => ({
+  type: LOAD_ADDRESSES_ERROR,
+  payload: { error },
+});
+
+export type State = {
+  addresses: string[],
+  error: string | null,
+};
+
+const initialState: State = {
+  addresses: [],
+  error: null,
+};
+
+export default (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case LOAD_ADDRESSES_SUCCESS:
+      return {
+        error: null,
+        addresses: action.payload.addresses,
+      };
+    case LOAD_ADDRESSES_ERROR:
+      return {
+        error: action.payload.error,
+        addresses: [],
+      };
+    default:
+      return state;
+  }
+};
--- app/redux/modules/reducer.js
+++ app/redux/modules/reducer.js
@@ -7,10 +7,12 @@ import type { RouterHistory } from 'react-router-dom';
 import wallet from './wallet';
 import transactions from './transactions';
 import send from './send';
+import receive from './receive';
 
 export const createRootReducer = (history: RouterHistory) => combineReducers({
   walletSummary: wallet,
   transactions,
   sendStatus: send,
+  receive,
   router: connectRouter(history),
 });
