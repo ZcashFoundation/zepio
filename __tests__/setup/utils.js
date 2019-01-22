19361ae3c7ccb921ea6d829e7343975ffc66f27f
George Lima <georgelima11@hotmail.com>
2019-01-22 15:48:54 -0300

    chore(test): add quitTimeout in Application

--- __tests__/setup/utils.js
+++ __tests__/setup/utils.js
@@ -1,13 +1,14 @@
 // @flow
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import electron from 'electron';
 import { Application } from 'spectron';
 
-export const TIMEOUT = 10000;
+export const TIMEOUT = 20000;
 
 export const getApp = () => new Application({
   path: electron,
   args: ['.'],
   startTimeout: TIMEOUT,
   waitTimeout: TIMEOUT,
+  quitTimeout: TIMEOUT,
 });
