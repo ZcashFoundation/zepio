eaa0ca3f6aaec545b8a904bbd101394b40f2d397
George Lima <georgelima11@hotmail.com>
2019-01-30 21:21:22 -0300

    feat(redux): add App redux module

--- app/types/app-state.js
+++ app/types/app-state.js
@@ -4,10 +4,12 @@ import type { State as WalletSummaryState } from '../redux/modules/wallet';
 import type { State as TransactionsState } from '../redux/modules/transactions';
 import type { State as SendState } from '../redux/modules/send';
 import type { State as ReceiveState } from '../redux/modules/receive';
+import type { State as App } from '../redux/modules/app';
 
 export type AppState = {
   walletSummary: WalletSummaryState,
   transactions: TransactionsState,
   sendStatus: SendState,
   receive: ReceiveState,
+  app: App,
 };
