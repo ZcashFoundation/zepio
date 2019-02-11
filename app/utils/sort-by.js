96f508310ed57667a40ca0efd5755614f9dc32f4
George Lima <georgelima11@hotmail.com>
2019-02-11 10:43:24 -0300

    feat(utils): add sortByDescend

--- app/utils/sort-by.js
+++ app/utils/sort-by.js
@@ -1,5 +1,8 @@
 // @flow
-
 /* eslint-disable max-len */
+
 // $FlowFixMe
-export const sortBy = <T>(field: string) => (arr: T[]): T[] => arr.sort((a, b) => (a[field] < b[field] ? 1 : -1));
+export const sortBy = <T>(field: string) => (arr: T[]): T[] => arr.sort((a, b) => (a[field] > b[field] ? 1 : -1));
+
+// $FlowFixMe
+export const sortByDescend = <T>(field: string) => (arr: T[]): T[] => arr.sort((a, b) => (a[field] < b[field] ? 1 : -1));
