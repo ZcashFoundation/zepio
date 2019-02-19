950b8b3bfb23f884b0938505c17b4251812936f7
George Lima <georgelima11@hotmail.com>
2019-02-19 11:36:00 -0300

    test(send): fix match

--- __tests__/e2e/send.test.js
+++ __tests__/e2e/send.test.js
@@ -15,23 +15,25 @@ afterEach(() => app.stop());
 
 describe('Send', () => {
   test('should load "Send Page"', async () => {
-    expect(app.client.element('#send-wrapper')
-      .isVisible()).resolves.toEqual(true);
+    expect(app.client.element('#send-wrapper').isVisible()).resolves.toEqual(true);
   });
 
   test('should show Additional Options click', async () => {
-    expect(app.client.element('#send-wrapper #send-fee-wrapper')
-      .isVisible()).resolves.toEqual(false);
+    expect(app.client.element('#send-wrapper #send-fee-wrapper').isVisible()).resolves.toEqual(
+      false,
+    );
 
     await app.client.element('#send-show-additional-options-button').click();
 
-    expect(app.client.element('#send-wrapper #send-fee-wrapper')
-      .isVisible()).resolves.toEqual(true);
+    expect(app.client.element('#send-wrapper #send-fee-wrapper').isVisible()).resolves.toEqual(
+      true,
+    );
   });
 
   test('should disable send button if required fields are empty', async () => {
-    expect(app.client.element('#send-submit-button')
-      .getAttribute('disabled')).resolves.toEqual(true);
+    expect(app.client.element('#send-submit-button').getAttribute('disabled')).resolves.toEqual(
+      true,
+    );
   });
 
   test('should enable send button if required fields are filled', async () => {
@@ -68,8 +70,9 @@ describe('Send', () => {
 
     await app.client.element('#send-submit-button').click();
 
-    expect(app.client.element('#send-confirm-transaction-modal')
-      .isVisible()).resolves.toEqual(true);
+    expect(app.client.element('#send-confirm-transaction-modal').isVisible()).resolves.toEqual(
+      true,
+    );
   });
 
   test('should display a load indicator while the transaction is processed', async () => {
@@ -91,15 +94,14 @@ describe('Send', () => {
     expect(app.client.getAttribute('#send-confirm-transaction-modal img', 'src')).resolves.toEqual(
       expect.stringContaining('/assets/sync_icon.png'),
     );
-    expect(app.client.getText('#send-confirm-transaction-modal p'))
-      .resolves.toEqual('Processing transaction...');
-    expect(app.client.element('#confirm-modal-button')
-      .isVisible()).resolves.toEqual(false);
+    expect(app.client.getText('#send-confirm-transaction-modal p')).resolves.toEqual(
+      'Processing transaction...',
+    );
+    expect(app.client.element('#confirm-modal-button').isVisible()).resolves.toEqual(false);
   });
 
   test('should show an error in invalid transaction', async () => {
-    expect(app.client.element('#send-error-text')
-      .isVisible()).resolves.toEqual(false);
+    expect(app.client.element('#send-error-text').isVisible()).resolves.toEqual(false);
 
     await app.client.element('#sidebar a:nth-child(1)').click();
     await app.client.element('#sidebar a:nth-child(2)').click();
@@ -124,8 +126,7 @@ describe('Send', () => {
   });
 
   test('should show a success screen after transaction and show a transaction item', async () => {
-    expect(app.client.element('#send-success-wrapper')
-      .isVisible()).resolves.toEqual(false);
+    expect(app.client.element('#send-success-wrapper').isVisible()).resolves.toEqual(false);
 
     await app.client.element('#sidebar a:nth-child(1)').click();
     await app.client.element('#sidebar a:nth-child(2)').click();
@@ -149,10 +150,11 @@ describe('Send', () => {
 
     await app.client.waitUntilTextExists('#transaction-item-operation-id-1', 'Send');
 
-    expect(await app.client.element('#transaction-item-operation-id-1 img')
-      .isVisible()).toEqual(true);
+    expect(await app.client.element('#transaction-item-operation-id-1 img').isVisible()).toEqual(
+      true,
+    );
     expect(
       await app.client.element('#transaction-item-operation-id-1 img').getAttribute('src'),
-    ).toEndWith('/assets/transaction_sent_icon.svg');
+    ).toEndWith('/assets/transaction_sent_icon_dark.svg');
   });
 });
