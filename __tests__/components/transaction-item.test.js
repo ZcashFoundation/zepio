c3f5e5293b1ea3e9013976e8ab50abd8001d2f0e
georgelima <georgelima11@hotmail.com>
2019-03-05 16:46:04 -0300

    test(snapshots): fix TransactionItem test

--- __tests__/components/transaction-item.test.js
+++ __tests__/components/transaction-item.test.js
@@ -19,7 +19,7 @@ describe('<TransactionItem />', () => {
           address='123456789123456789123456789123456789'
           transactionId='a0s9dujo23j0'
           amount={0.8652}
-          date='2019-02-20T19:31:57.117Z'
+          date={new Date(Date.UTC(2018, 10, 20, 1, 20, 35)).toString()}
           zecPrice={2.94}
           fees={0.0001}
         />
